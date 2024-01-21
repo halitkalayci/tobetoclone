09d3a81e8326e405c97d0954e71ea69c771b6e7d
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-21 13:56:33 +0300

    .

--- lib/models/lesson.dart
+++ lib/models/lesson.dart
@@ -2,4 +2,9 @@ class LessonModel {
   String name;
   String instructor;
   List<DateTime>? dates;
+  LessonModel({
+    required this.name,
+    required this.instructor,
+    this.dates,
+  });
 }
